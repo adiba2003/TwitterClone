8f0af9f34ec3bb29b4804f9536225e7f7dd8e0d2
Syeda Adiba Noshin <khatun.dilara711@gmail.com>
2026-08-13 19:32:39 +0600

    like class added

---
+++ TwitterClone/TwitterClone.Domain/Entities/Like.cs
@@ -0,0 +1,38 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Like
+    {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _likedAt;
+
+        public Like(Guid userId, Guid tweetId)
+        {
+            _id = Guid.NewGuid();
+            _userId = userId;
+            _tweetId = tweetId;
+            _likedAt = DateTime.UtcNow;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime LikedAt
+        {
+            get { return _likedAt; }
+        }
+    }
+}
