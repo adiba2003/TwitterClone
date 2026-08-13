ae8d83e1e761d07bb1d5b6e704491bd47881edce
Syeda Adiba Noshin <khatun.dilara711@gmail.com>
2026-08-13 19:36:31 +0600

    follow and retweet class added

---
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -0,0 +1,53 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Follow
+    {
+        private Guid _id;
+        private Guid _followerId;
+        private Guid _followingId;
+        private bool _isAccepted;
+        private DateTime _followedAt;
+        private DateTime? _unfollowedAt;
+
+        public Follow(Guid followerId, Guid followingId)
+        {
+            _id = Guid.NewGuid();
+            _followerId = followerId;
+            _followingId = followingId;
+            _isAccepted = true;
+            _followedAt = DateTime.UtcNow;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid FollowerId
+        {
+            get { return _followerId; }
+        }
+
+        public Guid FollowingId
+        {
+            get { return _followingId; }
+        }
+
+        public bool IsAccepted
+        {
+            get { return _isAccepted; }
+            set { _isAccepted = value; }
+        }
+
+        public DateTime FollowedAt
+        {
+            get { return _followedAt; }
+        }
+
+        public DateTime? UnfollowedAt
+        {
+            get { return _unfollowedAt; }
+            set { _unfollowedAt = value; }
+        }
+    }
+}
---
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -0,0 +1,45 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Retweet
+    {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private string _comment;
+        private DateTime _retweetedAt;
+
+        public Retweet(Guid userId, Guid tweetId)
+        {
+            _id = Guid.NewGuid();
+            _userId = userId;
+            _tweetId = tweetId;
+            _retweetedAt = DateTime.UtcNow;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set { _comment = value; }
+        }
+
+        public DateTime RetweetedAt
+        {
+            get { return _retweetedAt; }
+        }
+    }
+}
